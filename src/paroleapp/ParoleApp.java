bb99b4f7e3ecbd3995801b484ecc4069ceacc3a7
sandro.gallo <sandro.gallo@Labinfdoc.belluzzifioravanti.local>
2015-04-14 12:45:52 +0200

    Versione 2

--- src/paroleapp/ParoleApp.java
+++ src/paroleapp/ParoleApp.java
@@ -15,7 +15,7 @@ public class ParoleApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Sono la versione Set!");
+        System.out.println("Sono la versione Hash!");
     }
     
 }
